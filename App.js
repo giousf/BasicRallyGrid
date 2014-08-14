aa23ccfd87e316704e1de768f6744feb8216a274
Giovanni Sanchez <giovannisanchez@Giovannis-MacBook-Air.local>
2014-08-14 13:42:55 -0400

    our first grid working\!

---
+++ App.js
@@ -0,0 +1,47 @@
+// Custom Rally App that displays Stories in a grid
+//
+// Note: various console debugging messages intentionally kept in the code for learning purposes.
+
+
+Ext.define('CustomApp', {
+    extend: 'Rally.app.App',    // The parent class manages the app 'lifecycle' and calls launch () when ready.
+    componentCls: 'app',        //  CSS styles found in app.css
+           
+    // items:{ html:'<a href="https://help.rallydev.com/apps/2.0rc3/doc/">App SDK 2.0rc3 Docs</a>'},
+    launch: function() {
+           
+           console.log("Our first App !!"); // see console api:  https://developers.google.com/chrome-developer-tools/docs/console-api
+           this._loadData();                // we need to prefix with 'this.' so we call a method found at the app level.
+    },
+    
+    
+    //Get data from Rally
+    _loadData: function(){
+           
+           var myStore = Ext.create('Rally.data.wsapi.Store', {
+               model: 'User Story',
+               autoLoad: true,
+               listeners: {
+                  load: function(myStore, myData, success) {
+                     console.log('got data!',myStore, myData);
+                     this._loadGrid(myStore);   //if we did NOT pass scope:this below, this line would be incorrectly trying to call _createGrid() on the store which does not exist.
+                     },
+                     scope: this                // This tells the wsapi data store to forward pass along the app-level context into ALL listener functions
+                },
+                fetch: ['FormattedID','Name', 'ScheduleState', 'Owner']     // Look in the WSAPI docs online to see all fields available!
+            }); // End of myStore definition
+           
+           }, // End of _loadData function
+           
+    //Create and Show a Grid of given stories
+    _loadGrid: function(myStoryStore){
+           
+           var myGrid = Ext.create('Rally.ui.grid.Grid', {
+            store: myStoryStore,
+            columnCfgs: ['FormattedID','Name','Owner','ScheduleState']  // Columns to display;  must be the same names specified in the fetch: above in the wsapi data store
+            });
+           
+           this.add(myGrid);            // add the grid Component to the app-level Container (by doing this.add, it uses the app container
+           console.log('what is this: ',this);
+           }
+    });
